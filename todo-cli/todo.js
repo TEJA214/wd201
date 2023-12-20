7775239c7e9f187134b95a39f91a1370db4ae0b4
TEJA214 <teja@gmail.com>
2023-12-20 22:57:41 +0530

    main

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -80,3 +80,63 @@ module.exports = todoList;
 // let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
 // console.log(formattedItemsDueLater)
 // console.log("\n\n")
+
+// todo-cli/todo.js
+
+// const todoList = () => {
+//   const all = [];
+
+//   const add = (todoItem) => {
+//     all.push(todoItem);
+//   };
+
+//   const markAsComplete = (index) => {
+//     all[index].completed = true;
+//   };
+
+//   const overdue = () => {
+//     const today = new Date().toISOString().split('T')[0];
+//     return all.filter((item) => !item.completed && item.dueDate < today);
+//   };
+
+//   const dueToday = () => {
+//     const today = new Date().toISOString().split('T')[0];
+//     return all.filter(
+//       (item) =>
+//         item.dueDate === today &&
+//         (item.title === 'Pay rent' ? item.completed : true)
+//     );
+//   };
+
+//   const dueLater = () => {
+//     const today = new Date().toISOString().split('T')[0];
+//     return all.filter((item) => !item.completed && item.dueDate > today);
+//   };
+
+//   const toDisplayableList = () => {
+//     return all
+//       .map((item, index) => {
+//         const datePart =
+//           item.dueDate === formattedDate(new Date()) ? '' : item.dueDate;
+//         return [${item.completed ? 'x' : ' '}] ${index + 1}. ${item.title} ${datePart};
+//       })
+//       .join('\n');
+//   };
+
+//   return {
+//     all,
+//     add,
+//     markAsComplete,
+//     overdue,
+//     dueToday,
+//     dueLater,
+//     toDisplayableList,
+//   };
+// };
+
+// // Helper function for date formatting
+// const formattedDate = (date) => {
+//   return date.toISOString().split('T')[0];
+// };
+
+// module.exports = { todoList };
